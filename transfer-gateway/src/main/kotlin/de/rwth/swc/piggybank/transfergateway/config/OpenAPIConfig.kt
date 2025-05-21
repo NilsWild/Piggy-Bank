ad8106c6a662edef4a20faa733d6a28160803fe8
Nils Wild <nils.wild@swc.rwth-aachen.de>
2025-05-21 14:39:29 +0200

    feat: added openapi docs

---
+++ transfer-gateway/src/main/kotlin/de/rwth/swc/piggybank/transfergateway/config/OpenAPIConfig.kt
@@ -0,0 +1,41 @@
+package de.rwth.swc.piggybank.transfergateway.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Configuration for OpenAPI documentation.
+ */
+@Configuration
+class OpenAPIConfig {
+
+    /**
+     * Configures the OpenAPI documentation.
+     *
+     * @return The OpenAPI configuration
+     */
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Transfer Gateway API")
+                    .description("API for managing transfers in the Piggy Bank application")
+                    .version("1.0.0")
+                    .contact(
+                        Contact()
+                            .name("Piggy Bank Team")
+                            .email("piggybank@example.com")
+                    )
+            )
+            .addServersItem(
+                Server()
+                    .url("/")
+                    .description("Default Server URL")
+            )
+    }
+}
